dcf33653e3092ed4c98cb87f46db798c422f3d8c
Connor Szczepaniak <cszczepaniak@gmail.com>
2024-05-11 09:59:04 -0500

    more formatting

--- init.lua
+++ init.lua
@@ -616,7 +616,7 @@ require('lazy').setup({
     lazy = false,
     keys = {
       {
-        '<leader>f',
+        '<leader>ff',
         function()
           require('conform').format { async = true, lsp_fallback = true }
         end,
@@ -644,7 +644,9 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { { 'prettierd', 'prettier' } },
+        json = { { 'prettierd', 'prettier' } },
+        -- yaml = { 'yamlfix' },
       },
     },
   },
