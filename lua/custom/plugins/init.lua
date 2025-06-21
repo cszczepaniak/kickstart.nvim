52a351df5680f127d3bc38e4d5bb8a8e5a096e35
Connor Szczepaniak <cszczepaniak@gmail.com>
2025-06-21 12:31:34 -0500

    add some essential custom plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,28 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	{
+		"alexghergh/nvim-tmux-navigation",
+		config = function()
+			require("nvim-tmux-navigation").setup({})
+			vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", {})
+			vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", {})
+			vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", {})
+			vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", {})
+		end,
+	},
+	{
+		"akinsho/toggleterm.nvim",
+		config = function()
+			require("toggleterm").setup()
+			local terminal = require("toggleterm.terminal").Terminal
+			local lazygit = terminal:new({ direction = "float", cmd = "lazygit", hidden = true })
+
+			vim.api.nvim_create_user_command("LazyGitToggle", function()
+				lazygit:toggle()
+			end, {})
+			vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>LazyGitToggle<CR>", { noremap = true, silent = true })
+		end,
+	},
+}
